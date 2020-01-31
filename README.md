f6308afd1c2d99014fa87beac093cc1437fd9a28
M Yakub Mizan <ym@orchestra.ltd>
2020-02-01 00:39:06 +0600

    Code formatting and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# wp-check-if-plugin-active
-Check if a WordPress plugin is active
+# Check If a Plugin is Active
+
+On a WordPress site, plugins can be (1) activated on the site, (2) network activated on a multisite and (3) added as a must use plugin.  This class adds method to check if a plugin is active in all these cases. 
+
+
+Please, [read](https://yakub.xyz/the-right-way-to-check-if-a-plugin-is-active/) the associated blog post. 
+
+## Example 
+```
+// Check if WooCommerce is active.
+Utils_Plugins::is_active('woocommerce.php'); //True|False
+
+// Check if WooCommerce is network active.
+Utils_Plugins::is_network_active('woocommerce.php'); //True|False
+
+// Check if a plugin is added as must use (mu-plugins).
+Utils_Plugins::is_mu_active('plugin.php');  //True|False
--- plugin-utils.php
+++ plugin-utils.php
@@ -1,69 +1,71 @@
+<?php
+
 class Utils_Plugins {
 
 	/**
 	 * Check if a plugin is active
 	 *
 	 * @param string $plugin_main_file main file of the plugin, eg. woocommerce.php
-	 * @return bool True if plugin is active, false otherwise. 
+	 * @return bool True if plugin is active, false otherwise.
 	 */
 	public static function is_active( $plugin_main_file ) {
 		// get the list of plugins.
-        $active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+		$active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
 
-        // escape characters that have special meaning in regex.
-        $plugin_main_file = preg_quote( $plugin_main_file, '/' );
-        $is_plugin_installed = false;
+		// escape characters that have special meaning in regex.
+		$plugin_main_file    = preg_quote( $plugin_main_file, '/' );
+		$is_plugin_installed = false;
 
-        // Loop through the active plugins.
-        foreach( $active_plugins as $plugin ) {
-            if( preg_match( '/.+\/' . $plugin_main_file . '/', $plugin ) ) {
-                $is_plugin_installed = true;
-                break;
-            }
-        }
+		// Loop through the active plugins.
+		foreach ( $active_plugins as $plugin ) {
+			if ( preg_match( '/.+\/' . $plugin_main_file . '/', $plugin ) ) {
+				$is_plugin_installed = true;
+				break;
+			}
+		}
 
-        return $is_plugin_installed;
+		return $is_plugin_installed;
 	}
 
 	/**
 	 * Check if a plugin is network active
 	 *
 	 * @param string $plugin_main_file main file of the plugin, eg. woocommerce.php
-	 * @return bool True if plugin is active, false otherwise. 
+	 * @return bool True if plugin is active, false otherwise.
 	 */
 	public static function is_network_active( $plugin_main_file ) {
 
 		// if not a multisite, don't check.
 		if ( ! is_multisite() ) {
-	        return false;
-	    }
+			return false;
+		}
 
 		// get the list of plugins.
-        $active_plugins =  get_site_option( 'active_sitewide_plugins' );
+		$active_plugins = get_site_option( 'active_sitewide_plugins' );
 
-        // escape characters that have special meaning in regex.
-        $plugin_main_file = preg_quote( $plugin_main_file, '/' );
-        $is_plugin_active = false;
+		// escape characters that have special meaning in regex.
+		$plugin_main_file = preg_quote( $plugin_main_file, '/' );
+		$is_plugin_active = false;
 
-        // Loop through the active plugins.
-        foreach( $active_plugins as $plugin_name => $plugin_activation ) {
-            if( preg_match( '/.+\/' . $plugin_main_file . '/', $plugin_name ) ) {
-                $is_plugin_active = true;
-                break;
-            }
-        }
+		// Loop through the active plugins.
+		foreach ( $active_plugins as $plugin_name => $plugin_activation ) {
+			if ( preg_match( '/.+\/' . $plugin_main_file . '/', $plugin_name ) ) {
+				$is_plugin_active = true;
+				break;
+			}
+		}
 
-        return $is_plugin_active;
+		return $is_plugin_active;
 	}
 
 	/**
 	 * Check if a must use (mu) plugin exists.
 	 *
-	 * mu plugins are always active. So there's no need to check if they are 
+	 * mu plugins are always active. So there's no need to check if they are
 	 * active or not. We just need to check that they are in the list.
 	 *
 	 * @param string $plugin_main_file main file of the plugin, eg. woocommerce.php
-	 * @return bool True if plugin matches, false otherwise. 
+	 * @return bool True if plugin matches, false otherwise.
 	 */
 	public static function is_mu_active( $plugin_main_file ) {
 		$_mu_plugins = get_mu_plugins();
