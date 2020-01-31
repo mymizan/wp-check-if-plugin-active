54d9fd54ce237da6e1a0e3add31310f227f7649d
M Yakub Mizan <ctgcoder@gmail.com>
2020-01-31 23:17:21 +0600

    Create example-1.php

---
+++ example-1.php
@@ -0,0 +1,22 @@
+/** 
+ * Don't forget to set your prefix.
+ */ 
+function prefix_is_plugin_active( $plugin_main_file ) {
+
+		// get the list of plugins.
+        $active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+
+        // escape characters that have special meaning in regex.
+        $plugin_main_file = preg_quote( $plugin_main_file, '/' );
+        $is_plugin_installed = false;
+
+        // Loop through the active plugins.
+        foreach( $active_plugins as $plugin ) {
+            if( preg_match( '/.+\/' . $plugin_main_file . '/', $plugin ) ) {
+                $is_plugin_installed = true;
+                break;
+            }
+        }
+
+        return $is_plugin_installed;
+	}
